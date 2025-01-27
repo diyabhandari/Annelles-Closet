ef74f37d5fecfd9cf8a8104bd94e1c3f7de2f1cb
Diya Bhandari <diyabhandari@gmail.com>
2025-01-27 23:56:21 +0530

    add basic product card styles

--- src/features/product-list/ProductCard.jsx
+++ src/features/product-list/ProductCard.jsx
@@ -2,14 +2,14 @@ import styles from "./ProductList.module.css"
 
 export const ProductCard = ({product}) => {
   return(
-    <div className = "product-list-card">
-      <div className = "product-name">{product.name}</div>
-      <div className = "product-image">
-        <img src = {product.imageSrc} alt = {product.imageAlt} />
+    <div className = {styles["product-list-card"]}>
+      <div className = {styles["product-name"]}>{product.name}</div>
+      <div className = {styles["product-image-container"]}>
+        <img className = "product-image" src = {product.imageSrc} alt = {product.imageAlt} />
       </div>
-      <div className = "product-details">
-        <div className = "product-price">{product.price}</div>
-        <div className = "add-to-cart"></div>
+      <div className = {styles["product-details"]}>
+        <div className = {styles["product-price"]}>{product.price}</div>
+        <div className = {styles["add-to-cart"]}></div>
       </div>
     </div>
   )
--- src/features/product-list/ProductList.jsx
+++ src/features/product-list/ProductList.jsx
@@ -17,44 +17,46 @@ export const ProductList = () => {
     
     {
       id: 1,
-      name: 'Basic Tee',
+      name: 'Black Lace Tank',
       href: '#',
-      imageSrc: 'https://tailwindui.com/plus/img/ecommerce-images/product-page-01-related-product-01.jpg',
+      imageSrc: '/product-images/black-lace-tank.jpeg',
       imageAlt: "Front of men's Basic Tee in black.",
       price: '$35',
       color: 'Black',
     },
     {
       id: 2,
-      name: 'Basic Tee',
+      name: 'Cropped Tank',
       href: '#',
-      imageSrc: 'https://tailwindui.com/plus/img/ecommerce-images/product-page-01-related-product-01.jpg',
+      imageSrc: '/product-images/cropped-tank.jpeg',
       imageAlt: "Front of men's Basic Tee in black.",
-      price: '$35',
+      price: '$20',
       color: 'Black',
     },
     {
       id: 3,
-      name: 'Basic Tee',
+      name: 'White Lace Dress',
       href: '#',
-      imageSrc: 'https://tailwindui.com/plus/img/ecommerce-images/product-page-01-related-product-01.jpg',
+      imageSrc: '/product-images/white-lace-dress.jpeg',
       imageAlt: "Front of men's Basic Tee in black.",
       price: '$35',
-      color: 'Black',
+      color: 'White',
     },
     // More products...
   ]
 
   return (
-      <div className = "general-product-list">
-        <div className = "list-name">New Arrivals</div>
-        <div className = "list-viewer">
-          <div className = "left-scroll"></div> 
-          <div className = "list-content">
+      <div className = {styles["general-product-list"]}>
+        <div className = {styles["list-name"]}>
+          New Arrivals
+        </div>
+        <div className = {styles["list-viewer"]}>
+          <div className = {styles["left-scroll"]}></div> 
+          <div className = {styles["list-content"]}>
             {products.map((product) => (
               <ProductCard key = {product.id} product = {product}></ProductCard>
             ))}
-            <div className = "right-scroll"></div>
+            <div className = {styles["right-scroll"]}></div>
           </div>
         </div>
 
