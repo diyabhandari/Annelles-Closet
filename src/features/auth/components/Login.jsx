25587ffcae2ff4841b59ea504c39e2e88ce3d97c
Diya Bhandari <diyabhandari@gmail.com>
2025-02-27 00:01:43 +0530

    add login component css

--- src/features/auth/components/Login.jsx
+++ src/features/auth/components/Login.jsx
@@ -2,25 +2,27 @@ import styles from "../styles/Login.module.css"
 export const Login = () =>{
   return(
     <div className = {styles["container"]}>
-      <div className = {styles["logo"]}></div>
-      <div className = {styles["page-title"]}></div>
+      <div className = {styles["logo"]}>
+      <img href = "/" src = "./logo-dark.png" className = {styles["logo-image"]} />
+      </div>
+      <div className = {styles["page-title"]}>sign in to your account</div>
       <div className = {styles["inputs"]}>
         <div className = {styles["email"]}>
           <div className = {styles["field-title"]}>email address</div>
-          <div className = {styles["email-input-box input-box"]}></div>
+          <input type="email" className={styles["input-box"]} />
         </div>
         <div className = {styles["password"]}>
-          <div className = {styles["password-field-title field-title"]}>
-            <div>password</div>
+          <div className = {styles["password-field-titles"]}>
+            <div className = {styles["field-title"]}>password</div>
             <div className = {styles["forgot-password"]}>forgot password ?</div>
           </div>
-          <div className = {styles["password-input-box input-box"]}></div>
+          <input type="password" className={styles["input-box"]} />
         </div>
       </div>
-      <div className = {styles["sign-in-button"]}>sign in</div>
+      <button className = {styles["sign-in-button"]}>sign in</button>
       <div className = {styles["CTA"]}>
-        <p>not a member ?</p>
-        <p>create an account</p>
+        <p >not a member ?</p>
+        <p className = {styles["red-text"]}>create an account</p>
       </div>
     </div>
   )
