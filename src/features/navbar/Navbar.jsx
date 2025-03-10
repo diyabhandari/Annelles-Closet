bc0e46c2bae2eaff242a5432511c8abf70eee68f
Diya Bhandari <diyabhandari@gmail.com>
2025-03-10 22:41:48 +0530

    modify navbar colour

--- src/features/navbar/Navbar.jsx
+++ src/features/navbar/Navbar.jsx
@@ -43,8 +43,8 @@ export const Navbar = ({children}) => { //whatever component we pass as children
         <Link to="/cart">
           <div className= {styles["cart-icon"]}>
             <span class="fa-stack">
-              <i class="fa-solid fa-circle fa-stack-2x"></i>
-              <i class="fa-solid fa-cart-shopping fa-stack-1x fa-inverse"></i>
+              <i class="fa-solid fa-circle fa-stack-2x" className={styles["cart-bg"]}></i>
+              <i class="fa-solid fa-cart-shopping fa-stack-1x fa-inverse" className={styles["cart"]}></i>
             </span>
             <span className={styles["cart-badge"]}>3</span>
           </div>
