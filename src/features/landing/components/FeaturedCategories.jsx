bf43c28d0974ef57f839c87889f808b02e05a4f5
Diya Bhandari <diyabhandari@gmail.com>
2025-03-15 20:03:51 +0530

    create categories component with dummy href

--- src/features/landing/components/FeaturedCategories.jsx
+++ src/features/landing/components/FeaturedCategories.jsx
@@ -1,32 +1,33 @@
 import styles from "../styles/categories.module.css"
 import { CategoryCard } from "./CategoryCard"
+
 export const FeaturedCategories = () => {
   const categories = [
     {
       id: "C1",
       name: 'Accessories',
-      href: '#',
+      href: '/testing',
       imageSrc: 'https://i.postimg.cc/sXWtpSb5/category-accessories.png',
       imageAlt: "Hello kitty pair necklaces",
     },
     {
       id: "C2",
       name: 'Tops',
-      href: '#',
+      href: '/testing',
       imageSrc: 'https://i.postimg.cc/jjDVSBXg/category-tops.png',
       imageAlt: "Baby blue full sleeves top",
     },
     {
       id: "C3",
       name: 'Dresses',
-      href: '#',
+      href: '/testing',
       imageSrc: 'https://i.postimg.cc/prcXp47s/category-dresses.png',
       imageAlt: "Bold blue dress",
     },
     {
       id: "C4",
       name: 'Bottoms',
-      href: '#',
+      href: '/testing',
       imageSrc: 'https://i.postimg.cc/y8Q4B7F3/category-bottoms.png',
       imageAlt: "Yellow denim skirt",
     }
